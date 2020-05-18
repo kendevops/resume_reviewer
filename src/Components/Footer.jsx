11dfbca11ed56071be5d02c855038feaf23aaaa4
Kenneth Aladi <kennethmichael11@gmail.com>
2020-05-18 20:55:47 +0100

    Added content for the footer components

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,67 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer>
+      <div class="row">
+        <div class="twelve columns">
+          <ul class="social-links">
+            <li>
+              <a href="#">
+                <i class="fa fa-facebook"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-google-plus"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-linkedin"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-instagram"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-dribbble"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-skype"></i>
+              </a>
+            </li>
+          </ul>
+
+          <ul class="copyright">
+            <li>&copy; Copyright 2014 CeeVee</li>
+            <li>
+              Design by{" "}
+              <a title="Styleshout" href="http://www.styleshout.com/">
+                Styleshout
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div id="go-top">
+          <a class="smoothscroll" title="Back to Top" href="#home">
+            <i class="icon-up-open"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
   );
 };
 
